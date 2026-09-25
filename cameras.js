570993ca1a5edaf523b6155d8bff67b342dd7db5
godsun108 <283690x@gmail.com>
2026-09-25 13:47:47 -0400

    Open WINDOW to live Maui

--- cameras.js
+++ cameras.js
@@ -4,4 +4,5 @@ window.WINDOW_CAMERAS=[
  {type:"iframe",place:"Monterey Bay, California",tz:"America/Los_Angeles",status:"NEAR-LIVE",provider:"Monterey Bay Aquarium",embed:"https://www.youtube-nocookie.com/embed/fuCeRkeDxtQ?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://www.montereybayaquarium.org/cams-videos/live-cams/monterey-bay-cam"},
  {type:"iframe",place:"Open Sea · Monterey, California",tz:"America/Los_Angeles",status:"NEAR-LIVE",provider:"Monterey Bay Aquarium",embed:"https://www.youtube-nocookie.com/embed/nNTVZKz219U?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://www.montereybayaquarium.org/cams-videos/live-cams/open-sea-cam"}
  {type:"iframe",place:"Odaiba, Tokyo, Japan",tz:"Asia/Tokyo",status:"LIVE",provider:"Odaiba Live Camera",embed:"https://www.youtube-nocookie.com/embed/fdkVkWU99DI?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://www.youtube.com/watch?v=fdkVkWU99DI"},
+ {type:"iframe",place:"Maui, Hawaiʻi",tz:"Pacific/Honolulu",status:"LIVE",provider:"AlohaLiveCam",embed:"https://www.youtube-nocookie.com/embed/Nwnk_jmAMyk?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://www.youtube.com/watch?v=Nwnk_jmAMyk"},
 ];
