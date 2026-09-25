2bbbb4f0f3a113bbf85a30e64521d3dc45957559
godsun108 <283690x@gmail.com>
2026-09-25 13:48:52 -0400

    Open WINDOW to the Maunakea night sky

--- cameras.js
+++ cameras.js
@@ -6,4 +6,5 @@ window.WINDOW_CAMERAS=[
  {type:"iframe",place:"Odaiba, Tokyo, Japan",tz:"Asia/Tokyo",status:"LIVE",provider:"Odaiba Live Camera",embed:"https://www.youtube-nocookie.com/embed/fdkVkWU99DI?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://www.youtube.com/watch?v=fdkVkWU99DI"},
  {type:"iframe",place:"Maui, Hawaiʻi",tz:"Pacific/Honolulu",status:"LIVE",provider:"AlohaLiveCam",embed:"https://www.youtube-nocookie.com/embed/Nwnk_jmAMyk?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://www.youtube.com/watch?v=Nwnk_jmAMyk"},
  {type:"iframe",place:"Þorbjörn · Grindavík, Iceland",tz:"Atlantic/Reykjavik",status:"LIVE",provider:"AfarTV",embed:"https://www.youtube-nocookie.com/embed/bEOPDN710JQ?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://www.youtube.com/watch?v=bEOPDN710JQ"},
+ {type:"iframe",place:"Maunakea Summit · Hawaiʻi",tz:"Pacific/Honolulu",status:"LIVE",provider:"Subaru Telescope × Asahi",embed:"https://www.youtube-nocookie.com/embed/Iz0rFcyHf1k?autoplay=1&mute=1&controls=1&playsinline=1",source:"https://subarutelescope.org/en/news/topics/2021/04/21/2946.html"},
 ];
